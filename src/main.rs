917a6ef932751611f40030a775d0f34af4a556ff
Quincy West <rakaneth@hotmail.com>
2019-10-28 11:55:47 -0400

    controllable @ with moving @s

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,9 @@ struct Renderable {
 #[derive(Component)]
 struct Mobile {}
 
+#[derive(Component, Debug)]
+struct Player {}
+
 struct State{
     ecs: World
 }
@@ -49,11 +52,46 @@ impl State {
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
+        player_input(self, ctx);
         self.run_systems();
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
         for (pos, render) in (&positions, &renderables).join() {
             ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+        }
+    }
+}
+
+fn clamp<T: PartialOrd> (val: T, low: T, high: T) -> T {
+    match &val {
+        x if x < &low => low,
+        x if x > &high => high,
+        _ => val
+    }
+}
+
+fn try_move_player(dx: i32, dy: i32, ecs: &mut World) {
+    let mut positions = ecs.write_storage::<Position>();
+    let mut players = ecs.write_storage::<Player>();
+    for (_player, pos) in (&mut players, &mut positions).join() {
+        pos.x = clamp(pos.x + dx, 0, 100);
+        pos.y = clamp(pos.y + dy, 0, 40);
+    }
+}
+
+fn player_input(gs: &mut State, ctx: &mut Rltk) {
+    match ctx.key {
+        None => {}
+        Some (key) => match key {
+            VirtualKeyCode::Numpad8 => try_move_player(0, -1, &mut gs.ecs),
+            VirtualKeyCode::Numpad9 => try_move_player(1, -1, &mut gs.ecs),
+            VirtualKeyCode::Numpad6 => try_move_player(1, 0, &mut gs.ecs),
+            VirtualKeyCode::Numpad3 => try_move_player(1, 1, &mut gs.ecs),
+            VirtualKeyCode::Numpad2 => try_move_player(0, 1, &mut gs.ecs),
+            VirtualKeyCode::Numpad1 => try_move_player(-1, 1, &mut gs.ecs),
+            VirtualKeyCode::Numpad4 => try_move_player(-1, 0, &mut gs.ecs),
+            VirtualKeyCode::Numpad7 => try_move_player(-1, -1, &mut gs.ecs),
+            _ => {}
         }
     }
 }
@@ -66,6 +104,7 @@ fn main() {
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
     gs.ecs.register::<Mobile>();
+    gs.ecs.register::<Player>();
     gs.ecs
         .create_entity()
         .with(Position{x: 5, y: 5})
@@ -74,6 +113,7 @@ fn main() {
             fg: RGB::from_u8(191, 121, 101),
             bg: RGB::named(rltk::BLACK),
         })
+        .with(Player{})
         .build();
     for i in 0..10 {
         gs.ecs
